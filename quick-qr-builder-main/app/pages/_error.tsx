7e0035f50354b022d82459881716c76993765363
Gilbertswash <146853195+Gilbertswash@users.noreply.github.com>
2025-09-14 19:59:53 -0600

    commit changes

--- quick-qr-builder-main/app/pages/_error.tsx
+++
@@ -1,7 +0,0 @@
-export default function ErrorPage() {
-  return (
-    <main style={{minHeight:'100vh',display:'grid',placeItems:'center',fontFamily:'system-ui'}}>
-      <p>Something went wrong.</p>
-    </main>
-  );
-}
