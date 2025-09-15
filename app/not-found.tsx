bf6b8f1bc10fb251d913cb1ab5a2e441624f2dd3
Gilbertswash <146853195+Gilbertswash@users.noreply.github.com>
2025-09-14 20:44:55 -0600

    Delete app directory

--- app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <main style={{minHeight:'100vh',display:'grid',placeItems:'center',fontFamily:'system-ui'}}>
-      <p>Page not found.</p>
-    </main>
-  );
-}
